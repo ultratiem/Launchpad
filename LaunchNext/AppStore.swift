aa98ab2aaca7695bebea276a033aa1c3a647e0ac
IMCSER <85817538+RoversX@users.noreply.github.com>
2025-09-28 10:51:36 +1000

    remove F4

--- LaunchNext/AppStore.swift
+++ LaunchNext/AppStore.swift
@@ -736,7 +736,7 @@ final class AppStore: ObservableObject {
         return UserDefaults.standard.bool(forKey: rememberPageKey)
     }
 
-    @Published var globalHotKey: HotKeyConfiguration? = AppStore.loadHotKeyConfiguration() ?? HotKeyConfiguration(keyCode: 118, modifierFlags: []) {
+    @Published var globalHotKey: HotKeyConfiguration? = AppStore.loadHotKeyConfiguration() {
         didSet {
             persistHotKeyConfiguration()
             AppDelegate.shared?.updateGlobalHotKey(configuration: globalHotKey)
